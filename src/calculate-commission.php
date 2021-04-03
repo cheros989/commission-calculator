f628c84a65df8183ae30b2bcde35064b38951b2c
Roman Pavliukov <roman.pavliukov@gmail.com>
2021-04-04 02:55:28 +0300

    Fetch rates from api

--- src/calculate-commission.php
+++ src/calculate-commission.php
@@ -9,13 +9,11 @@ $reader = new \PhpOffice\PhpSpreadsheet\Reader\Csv();
 $filename = $argv[1];
 
 $exchangeApiClient = new \App\Client\ExchangeApiClient($_ENV['EXCHANGE_REST_API_KEY']);
-//$response = $exchangeApiClient->getLatestCurrenciesRates($_ENV['BASE_CURRENCY'], explode(',', $_ENV['SUPPORTED_CURRENCIES']));
-//$rates = $response['rates'];
+$response = $exchangeApiClient->getLatestCurrenciesRates($_ENV['BASE_CURRENCY'], explode(',', $_ENV['SUPPORTED_CURRENCIES']));
+$rates = $response['rates'];
+//$rates = ['USD' => 1.1497, 'JPY' => 129.53];
 $spreadsheet = $reader->load($filename);
-$calculationService = new \App\Service\CalculationService([
-    'USD' => 1.1497,
-    'JPY' => 129.53,
-], $_ENV['BASE_CURRENCY']); // Change base currency if you want
+$calculationService = new \App\Service\CalculationService($rates, $_ENV['BASE_CURRENCY']); // Change base currency if you want
 $inputProcessor = new \App\DataProcessor\CsvDataProcessor($calculationService, $spreadsheet);
 
 $inputProcessor->process();
